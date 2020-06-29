e768101ebb0c6e306c6b5f553c0b4c45f6ea4ea6
sianwa11 <sianwa.atemi@strathmore.edu>
2020-06-29 13:24:11 +0300

    Prefabs

---
+++ Assets/Scripts/Collider.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Collider : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            Debug.Log("Enemy !!");
+            // play death sound
+            // destroy
+            Destroy(collision.gameObject);
+
+            // respawn
+            spawnEnemy();
+        }
+
+        if (collision.gameObject.tag == "Booster")
+        {
+            Debug.Log("Booster !!");
+            // play powerup sound
+            //destroy
+            Destroy(collision.gameObject);
+        }
+    }
+
+    void spawnEnemy()
+    {
+
+    }
+}
